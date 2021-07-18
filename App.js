83b4ab42240e265ecdf730fe9df69ff07a6a7ee4
mich <mich-cook@users.noreply.github.com>
2021-07-18 15:45:34 -0700

    styles for heading and paragraph text. adding paragraph-styled Text element for styling

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello react native via expo</Text>
+      <Text style={styles.heading}>Hello react native via expo</Text>
+      <Text style={styles.paragraph}>Here is an image from a cartoon:</Text>
       <Image style={styles.image} source={require('./assets/images/apple-onion-block-party.jpg')} />
       <StatusBar style="auto" />
     </View>
@@ -22,5 +23,15 @@ const styles = StyleSheet.create({
   image: {
     height: '50%',
     width: '50%',
+  },
+  heading: {
+    "color": "#0077cc",
+    "fontSize": 40,
+    "fontWeight": "bold",
+  },
+  paragraph: {
+    "fontSize": 24,
+    "marginTop": 12,
+    "marginBottom": 12,
   },
 });
