eba61e2d6384a3da32bfb1b5851937e6d327fd75
mich <mich-cook@users.noreply.github.com>
2021-07-18 15:37:24 -0700

    different sample text for now

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello react native via expo</Text>
       <Image style={styles.image} source={require('./assets/images/apple-onion-block-party.jpg')} />
       <StatusBar style="auto" />
     </View>
