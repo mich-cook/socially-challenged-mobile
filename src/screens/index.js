08b35a4452275bad64a22395da9ec4e357ee5242
mich <mich-cook@users.noreply.github.com>
2021-07-18 21:39:47 -0700

    add icons to the tabs for a bit of friendliness

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
 
 // tabbed screens
 import AboutScreen from './about.js';
@@ -21,18 +22,27 @@ const TabNavigation = createBottomTabNavigator({
     screen: AboutScreen,
     navigationOptions: {
       tabBarLabel: 'About',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="information-circle-outline" size={24} color={tintColor} />
+      ),
     },
   },
   PrivacyTab: {
     screen: PrivacyScreen,
     navigationOptions: {
       tabBarLabel: 'Privacy',
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialIcons name="privacy-tip" size={24} color={tintColor} />
+      ),
     },
   },
   ChallengesTab: {
     screen: ChallengeStack,
     navigationOptions: {
       tabBarLabel: 'Challenges',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="barbell" size={24} color={tintColor} />
+      ),
     },
   },
 });
