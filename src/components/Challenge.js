aba3c1e00c293f390857b4887bbb98366d44ca6d
mich <mich-cook@users.noreply.github.com>
2021-07-20 22:51:58 -0700

    display formatted date for challenges

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, Scrollview } from 'react-native';
 import styled from 'styled-components/native';
+import { format } from 'date-fns';
 
 const ChallengeView = styled.ScrollView`
   padding: 10px;
@@ -9,7 +10,7 @@ const ChallengeView = styled.ScrollView`
 export default (props) => {
   return (
     <ChallengeView>
-      <Text>Challenge {props.challenge.id} created at: {props.challenge.createdAt}</Text>
+      <Text>Challenge {props.challenge.id} created at: {format(new Date(props.challenge.createdAt), 'MMM do, yyyy')}</Text>
       <Text>Created by: {props.challenge.owner.displayName}</Text>
     </ChallengeView>
   );
