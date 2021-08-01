317f481adbff6218d44ab4d70d3fe96af6d570e6
mich <mich-cook@users.noreply.github.com>
2021-08-01 15:07:12 -0700

    use signing form component in sign in screen

--- src/screens/signin.js
+++ src/screens/signin.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, Button, Text } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
+import SignInForm from '../components/SignInForm.js';
+
 const SignIn = props => {
   return (
     <View>
-      <Button title="Sign In" />
+      <SignInForm />
     </View>
   );
 };
